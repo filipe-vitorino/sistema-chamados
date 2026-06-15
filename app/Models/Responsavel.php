557a02f736fa0523ef13fb61f50fcc01e2c7a189
filipe-vitorino <filipe0vit@gmail.com>
2026-06-15 05:49:31 -0300

    criacao responsavel model

--- app/Models/Responsavel.php
+++ app/Models/Responsavel.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Responsavel extends Model
 {
-    //
+
+    protected $table = 'responsaveis';
+
+    protected $fillable = [
+        'nome',
+        'ativo'
+    ];
 }
