f6e4b16aaa94d66eeedfd78d93bc14a848d768a5
filipe-vitorino <filipe0vit@gmail.com>
2026-06-15 05:54:02 -0300

    criacao de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResponsavelController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource(
+    'responsaveis',
+    ResponsavelController::class
+);
