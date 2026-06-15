dd4132797a2a4770058c21273aab269b6eeed08a
filipe-vitorino <filipe0vit@gmail.com>
2026-06-15 18:24:10 -0300

    criacao migration de chamado

---
+++ database/migrations/2026_06_15_211842_create_chamados_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chamados', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('titulo');
+
+            $table->text('descricao');
+
+            $table->enum('prioridade', [
+                'baixa',
+                'media',
+                'alta'
+            ]);
+
+            $table->enum('status', [
+                'aberto',
+                'em_andamento',
+                'resolvido'
+            ]);
+
+            $table->foreignId('responsavel_id')
+                ->nullable()
+                ->constrained('responsaveis');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chamados');
+    }
+};
