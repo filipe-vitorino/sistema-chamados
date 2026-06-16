2e404aa8c9ff26ea000ff2d48397f5c5091fe945
filipe-vitorino <filipe0vit@gmail.com>
2026-06-15 21:13:09 -0300

    dashboard view

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sistema de Chamados</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container mt-5">
+
+    <h1 class="mb-4">
+        Sistema de Chamados
+    </h1>
+
+    <div class="row">
+
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+
+                    <h5>Responsáveis Ativos</h5>
+
+                    <h2>0</h2>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+
+                    <h5>Chamados Abertos</h5>
+
+                    <h2>0</h2>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+
+                    <h5>Em Andamento</h5>
+
+                    <h2>0</h2>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+
+                    <h5>Resolvidos</h5>
+
+                    <h2>0</h2>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
